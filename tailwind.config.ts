962aa9ac2638ef316604e770db6fc0e14e795bbf
Okpainmo <okpainmoandrew@gmail.com>
2024-10-24 13:45:29 +0100

    refactor: tailwindcss customizations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,36 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      // fontSize: {
+      //   heading: ['48px', '64px', '36px', '24px', '18px', '16px'] // h1 to h6
+      // }
     },
+    screens: {
+      xsm: '450px',
+      // => @media (min-width: 450px) { ... }
+
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 770px) { ... }
+
+      lg: '1150px',
+      // => @media (min-width: 1150px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px'
+      // => @media (min-width: 1536px) { ... }
+    }
   },
-  plugins: [],
 };
 export default config;
