3e7df8411adabc48c5439b3e55510f977b342abd
Okpainmo <okpainmoandrew@gmail.com>
2024-10-24 14:48:40 +0100

    refactor: login page updates to fix build error

---
+++ app/(routes)/(auth)/log-in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LogIn() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      Login Page
+    </div>
+  );
+}
+
+export default LogIn;
