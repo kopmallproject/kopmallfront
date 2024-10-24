b144ec66cae641d82a84b6adf2c3775f56070013
Okpainmo <okpainmoandrew@gmail.com>
2024-10-24 13:21:53 +0100

    init: implemented a fresh and standard project initialization

---
+++ commitlint.config.mjs
@@ -0,0 +1,29 @@
+const commitTypes = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'init',
+        'config',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'bulk',
+        'delete',
+        'assets',
+        'progress'
+      ]
+    ]
+  }
+};
+
+export default commitTypes;
