0faee11ef9e23d8d6674a03da118361537b76583
ikechukwu powel <ikechukwupowei21@gmail.com>
2024-11-06 21:22:29 +0100

    docs: created layout structure for auth-section

---
+++ app/(routes)/(auth)/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next';
+import MainAuthSectionLayout from '../(auth)/components/layout/MainAuthSectionLayout';
+
+export const metadata: Metadata = {
+  title: 'Private Practice | Authentication section',
+  description:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, vitae.'
+  // icons: {
+  //   icon: '/favicon.ico' // Path to your PNG favicon
+  // }
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return <MainAuthSectionLayout>{children}</MainAuthSectionLayout>;
+}
