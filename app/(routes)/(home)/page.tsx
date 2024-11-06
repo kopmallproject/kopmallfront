4dd0c0b41bcd67b3af40fc6e71ffe634914b01af
ikechukwu powel <ikechukwupowei21@gmail.com>
2024-11-06 18:38:09 +0100

    refactor: build the global project layout

--- app/(routes)/(home)/page.tsx
+++ app/(routes)/(home)/page.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import MainAppLayout from '@/app/global-components/layout/MainAppLayout';
 
 function Home() {
   return (
-    <main className="flex items-center justify-center flex-col min-h-screen">
-      <div className="nunito_sans">
-        Welcome to the KOP Mall project home page
-      </div>
-      <div className="poppins font-bold text-blue-500">Hello World!</div>
-    </main>
+    <MainAppLayout>
+      <main className=" min-h-screen">
+        {/* <div className="nunito_sans">
+          Welcome to the KOP Mall project home page
+        </div>
+        <div className="poppins font-bold text-blue-500">Hello World!</div> */}
+      </main>
+    </MainAppLayout>
   );
 }
 
