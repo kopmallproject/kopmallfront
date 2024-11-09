1097fb9d1a8239684289d29efe360b1fff1cb746
Okpainmo <okpainmoandrew@gmail.com>
2024-11-09 10:25:41 +0100

    chore: dev data, styling update(s) and new project favicon

---
+++ app/data/official-stores.ts
@@ -0,0 +1,63 @@
+import Image1 from '@/app/assets/images/img-24.png';
+import Image2 from '@/app/assets/images/img-25.png';
+import Image3 from '@/app/assets/images/img-26.png';
+import Image4 from '@/app/assets/images/img-27.png';
+import Image5 from '@/app/assets/images/img-28.png';
+import Image6 from '@/app/assets/images/img-29.png';
+import Image7 from '@/app/assets/images/img-30.png';
+import Image8 from '@/app/assets/images/img-31.png';
+import Image9 from '@/app/assets/images/img-32.png';
+import Image10 from '@/app/assets/images/img-33.png';
+import Image11 from '@/app/assets/images/img-34.png';
+import Image12 from '@/app/assets/images/img-35.png';
+
+export const officialStores = [
+  {
+    storeName: '',
+    storeImage: Image1
+  },
+  {
+    storeName: '',
+    storeImage: Image2
+  },
+  {
+    storeName: '',
+    storeImage: Image3
+  },
+  {
+    storeName: '',
+    storeImage: Image4
+  },
+  {
+    storeName: '',
+    storeImage: Image5
+  },
+  {
+    storeName: '',
+    storeImage: Image6
+  },
+  {
+    storeName: '',
+    storeImage: Image7
+  },
+  {
+    storeName: '',
+    storeImage: Image8
+  },
+  {
+    storeName: '',
+    storeImage: Image9
+  },
+  {
+    storeName: '',
+    storeImage: Image10
+  },
+  {
+    storeName: '',
+    storeImage: Image11
+  },
+  {
+    storeName: '',
+    storeImage: Image12
+  }
+];
