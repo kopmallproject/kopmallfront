41ceb7e31da78f781c4d0d5f985dea49d7445987
ikechukwu powel <ikechukwupowei21@gmail.com>
2024-11-12 20:52:56 +0100

    config: install react redux and headless ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,7 @@
 import type { Metadata } from 'next';
 import './styles/globals.css';
 import { nunito_sans, poppins } from './utils/font';
-// import { RTKProvider } from './rtk-base/provider';
-// import { SpeedInsights } from '@vercel/speed-insights/next';
-// import { Analytics } from '@vercel/analytics/react';
+import { Providers } from './rtk-base/provider';
 
 export default function RootLayout({
   children
@@ -13,10 +11,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    // <RTKProvider>
-    <html lang="en" className={`${nunito_sans} ${poppins}`}>
-      <body className="nunito_sans">{children}</body>
-    </html>
-    // </RTKProvider>
+    <Providers>
+      <html lang="en" className={`${nunito_sans} ${poppins}`}>
+        <body className="nunito_sans">{children}</body>
+      </html>
+    </Providers>
   );
 }
