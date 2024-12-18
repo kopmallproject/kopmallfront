15c6e66a726a3c37b6798c3486a000cf9fd1a18f
Okpainmo <okpainmoandrew@gmail.com>
2024-12-18 16:31:28 +0100

    chore: meta-data update... Google ad-sense for client

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,15 @@ import './styles/globals.css';
 import { nunito_sans, poppins } from './utils/font';
 import { Providers } from './rtk-base/provider';
 
+export const metadata: Metadata = {
+  metadataBase: new URL('https://kingofpimall.com/'),
+  title: 'King Of Pi Mall',
+  description: 'Enjoy online shopping and pay with Pi cryptocurrency',
+  other: {
+    'google-adsense-account': 'ca-pub-1063404400059938'
+  }
+};
+
 export default function RootLayout({
   children
 }: {
